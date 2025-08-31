beebc7ac59144383e171caaa99017007b5e4973f
reshav1234 <thapareshav50@gmail.com>
2025-08-31 10:40:55 +1000

    multipe client connection

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 
 func main() {
 	// creating tcp connection
+
+	messageChannel := make(chan []byte, 10)
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -20,35 +22,39 @@ func main() {
 
 	fmt.Print("server started")
 
+	go func() {
+		for msg := range messageChannel {
+			fmt.Println("Received message:", string(msg))
+		}
+	}()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting", err)
 			continue
 		}
-		go handleClient(conn)
+		go handleClient(conn, messageChannel)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, messageChannel chan []byte) {
 	defer conn.Close()
 
 	// buffer to read input
-
 	buffer := make([]byte, 1024)
 	for {
 		readBuffer, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println("Error reading buffer: ", err)
-			return
+			continue
 		}
-		fmt.Printf("Server: %s\n", buffer[:readBuffer])
+		messageChannel <- buffer[:readBuffer]
 
-		data := []byte("client:")
-		_, err = conn.Write(data)
-		if err != nil {
-			fmt.Print("Error writing in server: ", err)
-			return
-		}
+		//	data := []byte("client:")
+		//	_, err = conn.Write(data)
+		//	if err != nil {
+		//		fmt.Print("Error writing in server: ", err)
+		//		return
+		//	}
 	}
 }
